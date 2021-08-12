ac7b33f675a9d4e2e660f9e3b18f599a52b8588a
Nicolas Correia <nicononoya@gmail.com>
2021-08-12 19:47:09 -0300

    Testing API requests

--- src/main/java/com/cliente/entrega/controller/ClienteController.java
+++ src/main/java/com/cliente/entrega/controller/ClienteController.java
@@ -1,12 +1,29 @@
 package com.cliente.entrega.controller;
 
+import java.util.Arrays;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cliente.entrega.model.Cliente;
 
 @RestController
 public class ClienteController {
 	@GetMapping("/clientes")
-	public String listar() {
-		return "Testar requisição";
+	public List<Cliente> listar() {
+		Cliente cliente1 = new Cliente();
+		cliente1.setId(2L);
+		cliente1.setNome("Nicolas");
+		cliente1.setEmail("sdksld@gmail.com");
+		cliente1.setTelefone("99895644545");
+		
+		Cliente cliente2 = new Cliente();
+		cliente2.setId(5L);
+		cliente2.setNome("Lucas");
+		cliente2.setEmail("lelelsd@gmail.com");
+		cliente2.setTelefone("4848415");
+		
+		return Arrays.asList(cliente1, cliente2);
 	}
 }
