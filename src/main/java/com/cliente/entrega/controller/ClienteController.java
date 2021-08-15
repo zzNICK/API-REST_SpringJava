2fb70e30a1ad8af943e32197954d4c3926fdf9a0
Nicolas Correia <nicononoya@gmail.com>
2021-08-15 14:09:44 -0300

    Implementing JPA with Hibernate

--- src/main/java/com/cliente/entrega/controller/ClienteController.java
+++ src/main/java/com/cliente/entrega/controller/ClienteController.java
@@ -3,6 +3,9 @@ package com.cliente.entrega.controller;
 import java.util.Arrays;
 import java.util.List;
 
+import javax.persistence.EntityManager;	// Uma inteface para fazer operações com as entidades que serão refletidas depois no DB como um CRUD
+import javax.persistence.PersistenceContext;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -10,20 +13,13 @@ import com.cliente.entrega.model.Cliente;
 
 @RestController
 public class ClienteController {
+	
+	@PersistenceContext  //Serve para injetar um EntityManager na variável abaixo
+	private EntityManager entityM;
+	
 	@GetMapping("/clientes")
 	public List<Cliente> listar() {
-		Cliente cliente1 = new Cliente();
-		cliente1.setId(2L);
-		cliente1.setNome("Nicolas");
-		cliente1.setEmail("sdksld@gmail.com");
-		cliente1.setTelefone("99895644545");
-		
-		Cliente cliente2 = new Cliente();
-		cliente2.setId(5L);
-		cliente2.setNome("Lucas");
-		cliente2.setEmail("lelelsd@gmail.com");
-		cliente2.setTelefone("4848415");
-		
-		return Arrays.asList(cliente1, cliente2);
+		return entityM.createQuery("from Cliente", Cliente.class)
+				.getResultList();
 	}
 }
